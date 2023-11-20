2c8efa6e3aca2e32a2d1a5fabbd32be4863098be
bangcong.270419 <bangcong.270419@LAB-A-PC20>
2023-11-20 19:06:17 +0800

    Add project files.

---
+++ BasicCalculator/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace BasicCalculator
+{
+    partial class FrmBasicCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtbox1 = new System.Windows.Forms.TextBox();
+            this.txtbox2 = new System.Windows.Forms.TextBox();
+            this.cbOperator = new System.Windows.Forms.ComboBox();
+            this.btnCompute = new System.Windows.Forms.Button();
+            this.txtbox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label1.Location = new System.Drawing.Point(12, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(306, 52);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Basic Calculator";
+            // 
+            // txtbox1
+            // 
+            this.txtbox1.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbox1.Location = new System.Drawing.Point(64, 122);
+            this.txtbox1.Multiline = true;
+            this.txtbox1.Name = "txtbox1";
+            this.txtbox1.Size = new System.Drawing.Size(340, 50);
+            this.txtbox1.TabIndex = 1;
+            // 
+            // txtbox2
+            // 
+            this.txtbox2.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbox2.Location = new System.Drawing.Point(64, 248);
+            this.txtbox2.Multiline = true;
+            this.txtbox2.Name = "txtbox2";
+            this.txtbox2.Size = new System.Drawing.Size(340, 50);
+            this.txtbox2.TabIndex = 2;
+            // 
+            // cbOperator
+            // 
+            this.cbOperator.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbOperator.FormattingEnabled = true;
+            this.cbOperator.Location = new System.Drawing.Point(165, 187);
+            this.cbOperator.Name = "cbOperator";
+            this.cbOperator.Size = new System.Drawing.Size(121, 46);
+            this.cbOperator.TabIndex = 3;
+            // 
+            // btnCompute
+            // 
+            this.btnCompute.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCompute.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCompute.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnCompute.Location = new System.Drawing.Point(165, 485);
+            this.btnCompute.Name = "btnCompute";
+            this.btnCompute.Size = new System.Drawing.Size(138, 55);
+            this.btnCompute.TabIndex = 4;
+            this.btnCompute.Text = "Compute";
+            this.btnCompute.UseVisualStyleBackColor = true;
+            this.btnCompute.Click += new System.EventHandler(this.btnCompute_Click);
+            // 
+            // txtbox3
+            // 
+            this.txtbox3.BackColor = System.Drawing.SystemColors.InfoText;
+            this.txtbox3.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbox3.ForeColor = System.Drawing.Color.ForestGreen;
+            this.txtbox3.Location = new System.Drawing.Point(64, 323);
+            this.txtbox3.Multiline = true;
+            this.txtbox3.Name = "txtbox3";
+            this.txtbox3.Size = new System.Drawing.Size(340, 135);
+            this.txtbox3.TabIndex = 5;
+            this.txtbox3.Text = "Total:";
+            // 
+            // FrmBasicCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = global::BasicCalculator.Properties.Resources.db7;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(466, 552);
+            this.Controls.Add(this.txtbox3);
+            this.Controls.Add(this.btnCompute);
+            this.Controls.Add(this.cbOperator);
+            this.Controls.Add(this.txtbox2);
+            this.Controls.Add(this.txtbox1);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Name = "FrmBasicCalculator";
+            this.Text = "FrmBasicCalculator";
+            this.Load += new System.EventHandler(this.FrmBasicCalculator_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtbox1;
+        private System.Windows.Forms.TextBox txtbox2;
+        private System.Windows.Forms.ComboBox cbOperator;
+        private System.Windows.Forms.Button btnCompute;
+        private System.Windows.Forms.TextBox txtbox3;
+    }
+}
+
---
+++ BasicCalculator/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CalculatorPrivateAssembly;
+
+
+
+
+namespace BasicCalculator
+{
+    public partial class FrmBasicCalculator : Form
+    {
+        public FrmBasicCalculator()
+        {
+            InitializeComponent();
+        }
+
+        float Num1,Num2;
+
+        private void FrmBasicCalculator_Load(object sender, EventArgs e)
+        {
+
+            cbOperator.Items.AddRange(new object[] { "+", "-", "*", "/" });
+
+        }
+
+        private void btnCompute_Click(object sender, EventArgs e)
+        {
+
+            Num1 = float.Parse(txtbox1.Text);
+            Num2 = float.Parse(txtbox2.Text);
+
+
+            if(cbOperator.Text == "+")
+            {
+                txtbox3.Text = BasicComputation.Addition(Num1,Num2).ToString();
+
+            }
+            else if(cbOperator.Text == "-")
+            {
+                txtbox3.Text = BasicComputation.Subtraction(Num1, Num2).ToString();
+            }
+            else if(cbOperator.Text == "*")
+            {
+                txtbox3.Text = BasicComputation.Multiplication(Num1, Num2).ToString();
+            }
+            else if( cbOperator.Text == "/")
+            {
+                txtbox3.Text = BasicComputation.Division(Num1, Num2).ToString();
+            }
+
+        }
+
+
+    }
+}
